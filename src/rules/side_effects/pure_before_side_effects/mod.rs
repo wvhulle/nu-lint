bb5cb183e50d2667907a9b8c2194921d00e682bd
Willem Vanhulle <willemvanhulle@protonmail.com>
2025-11-13 22:56:30 +0100

    Improve effect organisation

--- src/rules/side_effects/pure_before_side_effects/mod.rs
+++ src/rules/side_effects/pure_before_side_effects/mod.rs
@@ -1,98 +1,14 @@
 use nu_protocol::{
-    BlockId, Category,
-    ast::{Block, Call, Expr, Expression, Pipeline},
+    BlockId,
+    ast::{Expr, Pipeline},
 };
 
 use crate::{
-    ast::{call::CallExt, expression::ExpressionExt},
+    ast::effect::{StatementType, classify_expression},
     context::LintContext,
     rule::{Rule, RuleCategory},
     violation::{RuleViolation, Severity},
 };
-
-const SIDE_EFFECT_COMMANDS: &[&str] = &[
-    "print",
-    "save",
-    "rm",
-    "mv",
-    "cp",
-    "mkdir",
-    "touch",
-    "cd",
-    "exit",
-    "error make",
-    "input",
-    "input list",
-];
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum StatementType {
-    Pure,
-    SideEffect,
-    Control,
-}
-
-fn is_side_effect_command(command_name: &str) -> bool {
-    SIDE_EFFECT_COMMANDS.contains(&command_name)
-}
-
-fn classify_expression(expr: &Expression, context: &LintContext) -> StatementType {
-    match &expr.expr {
-        Expr::ExternalCall(_, _) => StatementType::SideEffect,
-        Expr::Call(call) => classify_call(call, context),
-        Expr::Nothing => StatementType::Control,
-        _ => StatementType::Pure,
-    }
-}
-
-fn classify_call(call: &Call, context: &LintContext) -> StatementType {
-    if call.is_control_flow_command(context) {
-        return if has_side_effects_in_call(call, context) {
-            StatementType::SideEffect
-        } else {
-            StatementType::Control
-        };
-    }
-
-    let command_name = call.get_call_name(context);
-    if is_side_effect_command(&command_name) {
-        return StatementType::SideEffect;
-    }
-
-    let category = context
-        .working_set
-        .get_decl(call.decl_id)
-        .signature()
-        .category;
-    match category {
-        Category::FileSystem | Category::Network | Category::System => StatementType::SideEffect,
-        _ => StatementType::Pure,
-    }
-}
-
-fn has_side_effects_in_call(call: &Call, context: &LintContext) -> bool {
-    call.all_arg_expressions().iter().any(|arg_expr| {
-        if let Some(block_id) = arg_expr.extract_block_id() {
-            let block = context.working_set.get_block(block_id);
-            return has_side_effects_in_block(block, context);
-        }
-        matches!(
-            classify_expression(arg_expr, context),
-            StatementType::SideEffect
-        )
-    })
-}
-
-fn has_side_effects_in_block(block: &Block, context: &LintContext) -> bool {
-    block.pipelines.iter().any(|pipeline| {
-        pipeline.elements.iter().any(|elem| {
-            matches!(
-                classify_expression(&elem.expr, context),
-                StatementType::SideEffect
-            )
-        })
-    })
-}
 
 fn count_consecutive_pure_statements(pipelines: &[Pipeline], context: &LintContext) -> usize {
     pipelines

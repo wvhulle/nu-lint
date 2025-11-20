b81650305175cea75b4f0201a91344d1f1f6cecb
Willem Vanhulle <wvhulle@users.noreply.github.com>
2025-11-20 21:19:08 +0100

    Switch systemd journal prefix to ast (#47)
    * Switch systemd journal prefix to ast
    
    * Add automated fixes
    
    * Make fixes also suggest replacing numeric prefixes loglevel to keyword prefixes

--- src/rules/systemd_journal_prefix/detect_bad.rs
+++ src/rules/systemd_journal_prefix/detect_bad.rs
@@ -45,3 +45,27 @@ fn test_detect_print_with_variable() {
     let bad_code = r#"print $"Processing {$file}""#;
     rule().assert_detects(bad_code);
 }
+
+#[test]
+fn test_detect_numeric_prefix() {
+    let bad_code = r#"print "<6>Starting service""#;
+    rule().assert_detects(bad_code);
+}
+
+#[test]
+fn test_detect_all_numeric_prefixes() {
+    for level in 0..=7 {
+        let bad_code = format!(r#"print "<{level}>Test message""#);
+        rule().assert_detects(&bad_code);
+    }
+}
+
+#[test]
+fn test_detect_numeric_prefix_in_function() {
+    let bad_code = r#"
+def main [] {
+    print "<3>Error occurred"
+}
+"#;
+    rule().assert_detects(bad_code);
+}
--- src/rules/systemd_journal_prefix/ignore_good.rs
+++ src/rules/systemd_journal_prefix/ignore_good.rs
@@ -1,22 +1,6 @@
 use super::rule;
 
 #[test]
-fn test_ignore_numeric_log_levels() {
-    for (level, msg) in [
-        ("0", "System emergency"),
-        ("1", "Alert condition"),
-        ("2", "Critical failure"),
-        ("3", "Error occurred"),
-        ("4", "Warning: deprecated feature"),
-        ("5", "System notification"),
-        ("6", "Starting process"),
-        ("7", "Debug: entering function"),
-    ] {
-        rule().assert_ignores(&format!(r#"print "<{level}>{msg}""#));
-    }
-}
-
-#[test]
 fn test_ignore_keyword_log_levels() {
     for keyword in [
         "emerg", "alert", "crit", "err", "warning", "notice", "info", "debug",
@@ -26,21 +10,38 @@ fn test_ignore_keyword_log_levels() {
 }
 
 #[test]
-fn test_ignore_mixed_levels() {
+fn test_ignore_mixed_keyword_levels() {
     let good_code = r#"
-print "<6>Starting"
-print "<4>Warning detected"
-print "<3>Error occurred"
+print "<info>Starting"
+print "<warning>Warning detected"
+print "<err>Error occurred"
 "#;
     rule().assert_ignores(good_code);
 }
 
 #[test]
-fn test_ignore_mixed_keyword_and_numeric() {
-    let good_code = r#"
-print "<info>Starting"
-print "<4>Warning detected"
-print "<err>Error occurred"
-"#;
+fn test_ignore_interpolated_strings_with_keyword_prefix() {
+    let good_code = r#"print $"<info>Monitoring ($keyboard) for ($desc)""#;
     rule().assert_ignores(good_code);
+}
+
+#[test]
+fn test_ignore_interpolated_strings_keyword_levels() {
+    for keyword in [
+        "emerg", "alert", "crit", "err", "warning", "notice", "info", "debug",
+    ] {
+        rule().assert_ignores(&format!(r#"print $"<{keyword}>Process ($name) started""#));
+    }
+}
+
+#[test]
+fn test_ignore_interpolated_strings_multiple_vars() {
+    let good_code = r#"print $"<info>Task ($task_id) completed in ($duration)ms by ($user)""#;
+    rule().assert_ignores(good_code);
+}
+
+#[test]
+fn test_ignore_interpolated_strings_with_echo() {
+    rule().assert_ignores(r#"echo $"<info>Server ($hostname) is running""#);
+    rule().assert_ignores(r#"echo $"<warning>Connection from ($ip_addr)""#);
 }

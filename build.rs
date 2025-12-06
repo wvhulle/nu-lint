e36679cc111a047d37f1b6f21d516558ac4579af
Willem Vanhulle <willemvanhulle@protonmail.com>
2025-12-06 20:01:18 +0100

    Fix build script

--- build.rs
+++ build.rs
@@ -6,9 +6,12 @@ fn main() {
         .parse()
         .expect("Failed to parse Cargo.toml");
 
-    let version = cargo_toml["dependencies"]["nu-parser"]
+    let nu_parser_dep = &cargo_toml["dependencies"]["nu-parser"];
+
+    let version = nu_parser_dep
         .as_str()
-        .expect("nu-parser version should be a string");
+        .or_else(|| nu_parser_dep.get("version").and_then(|v| v.as_str()))
+        .expect("nu-parser version not found in Cargo.toml");
 
     println!("cargo:rustc-env=NU_PARSER_VERSION={version}");
     println!("cargo:rerun-if-changed=Cargo.toml");

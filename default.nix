905635ab0cb980fd15d29949cb08337375db8032
Willem Vanhulle <willemvanhulle@protonmail.com>
2025-12-07 21:16:26 +0100

    Fix toolchain not having sha

--- default.nix
+++ default.nix
@@ -14,6 +14,7 @@ let
 
   toolchain = fenix.fromToolchainFile {
     file = ./rust-toolchain.toml;
+    sha256 = "sha256-IQUcjhizZsNE1NYkdrwkVNxGpUlujMlfy8tdcbp7NnQ=";
   };
 
   rustPlatform = pkgs.makeRustPlatform {

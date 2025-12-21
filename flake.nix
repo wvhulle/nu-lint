8a3e5b5330374a9f07b545bbcfd08866afe67974
Willem Vanhulle <willemvanhulle@protonmail.com>
2025-12-21 23:47:27 +0100

    Switch to flakes

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    fenix.url = "github:nix-community/fenix";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      naersk,
+      fenix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        toolchain = fenix.packages.${system}.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "sha256-IQUcjhizZsNE1NYkdrwkVNxGpUlujMlfy8tdcbp7NnQ=";
+        };
+
+        naersk' = pkgs.callPackage naersk {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+      in
+      {
+        packages.default = naersk'.buildPackage {
+          src = ./.;
+          meta.mainProgram = "nu-lint";
+        };
+
+        apps.default = {
+          type = "app";
+          program = pkgs.lib.getExe self.packages.${system}.default;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = [ toolchain ];
+        };
+      }
+    );
+}
